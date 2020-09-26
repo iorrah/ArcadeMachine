726caa3926d7bfde2d96bc5549daa52f172d8e70
Iorrah Mota <iorrah.mota@gmail.com>
2020-09-26 17:59:33 +0200

    Add game dynamic

--- ArcadeMachine/ContentView.swift
+++ ArcadeMachine/ContentView.swift
@@ -8,45 +8,134 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var credits = 1000
+    @State private var symbols = ["apple", "donut", "pizza"]
+    @State private var numbers = [0, 0, 0]
+    @State private var bet = 5
+    @State private var won = false
+    
     var body: some View {
         ZStack {
-            Image("background")
-                .resizable()
+            Rectangle()
+                .foregroundColor(Color(red: 200/255, green: 143/255, blue: 32/255))
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-        
-            VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 20.0) {
+            
+            Rectangle()
+                .foregroundColor(Color(red: 228/255, green: 195/255, blue: 76/255))
+                .rotationEffect(Angle(degrees: 45))
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            
+            VStack {
                 
                 Spacer()
                 
-                Image("system")
-                
-                Text("Arcade Machine")
-                    .font(.title)
-                    .fontWeight(.heavy)
-                    .foregroundColor(Color.black)
-                    .multilineTextAlignment(.center)
+                // Title
+                HStack {
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                    
+                    Text("Machine Arcade")
+                        .bold()
+                        .foregroundColor(.white)
+                    
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                }.scaleEffect(2)
                 
                 Spacer()
                 
+                // Creadits
+                
+                VStack {
+                    HStack {
+                        Image(systemName: "checkmark.seal.fill")
+                            .foregroundColor(.green)
+                        
+                        Text("You won!")
+                            .bold()
+                            .foregroundColor(.black)
+                        
+                        Image(systemName: "checkmark.seal.fill")
+                            .foregroundColor(.green)
+                    }.opacity(self.won ? 1 : 0)
+                    
+                    Text("Credits: " + String(self.credits))
+                        .foregroundColor(.black)
+                        .padding(.all, 10)
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+                }
+                
+                Spacer()
+                
+                // Cards
+                HStack {
+                    Image(self.symbols[self.numbers[0]])
+                    .renderingMode(.original)
+                    .resizable()
+                    .frame(width: 100.0, height: 100.0)
+                    .padding(10)
+                    .background(Color.white.opacity(0.5))
+                    .cornerRadius(20)
+                    .shadow(color: /*@START_MENU_TOKEN@*/Color.gray/*@END_MENU_TOKEN@*/, radius: 3)
+                    
+                    Image(self.symbols[self.numbers[1]])
+                    .renderingMode(.original)
+                    .resizable()
+                    .frame(width: 100.0, height: 100.0)
+                    .padding(10)
+                    .background(Color.white.opacity(0.5))
+                    .cornerRadius(20)
+                    .shadow(color: Color.gray, radius: 3)
+                    
+                    Image(self.symbols[self.numbers[2]])
+                    .renderingMode(.original)
+                    .resizable()
+                    .frame(width: 100.0, height: 100.0)
+                    .padding(10)
+                    .background(Color.white.opacity(0.5))
+                    .cornerRadius(20)
+                    .shadow(color: /*@START_MENU_TOKEN@*/Color.gray/*@END_MENU_TOKEN@*/, radius: 3)
+                }
+                
+                Spacer()
+                
+                // Button
                 Button(action: {
                     
-                    // TODO
-                        
-                }, label: {
-                    Image("apple")
-                        .renderingMode(.original)
-                        .resizable()
-                        .frame(width: 100.0, height: 100.0)
-                        .padding()
-                        .background(Color.white)
-                        .cornerRadius(20)
-                        .shadow(color: /*@START_MENU_TOKEN@*/Color.gray/*@END_MENU_TOKEN@*/, radius: 3)
+                    self.credits += 1
                     
+                    // Shufle the images
+                    self.numbers[0] = Int.random(in: 0...self.symbols.count - 1)
+                    
+                    self.numbers[1] = Int.random(in: 0...self.symbols.count - 1)
+                    
+                    self.numbers[2] = Int.random(in: 0...self.symbols.count - 1)
+                    
+                    // Check winnings
+                    if self.numbers[0] == self.numbers[1] && self.numbers[1] == self.numbers[2] {
+                        // Won
+                        self.won = true
+                        self.credits += self.bet * 10
+                    } else {
+                        // Lost
+                        self.won = false
+                        self.credits -= self.bet
+                    }
+                    
+                }, label: {
+                    Text("Spin")
+                        .foregroundColor(.white)
+                        .bold()
+                        .padding(.all, 15)
+                        .padding([.leading, .trailing], 35)
+                        .background(Color.pink)
+                        .cornerRadius(10)
                 })
                 
                 Spacer()
-                
             }
         }
     }
---
+++ ArcadeMachine/ContentView_.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  ArcadeMachine
+//
+//  Created by Iorrah Gonzalez on 26.09.20.
+//
+
+import SwiftUI
+
+struct ContentView_: View {
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+        
+            VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 20.0) {
+                
+                Spacer()
+                
+                Text("Arcade Machine")
+                    .font(.title)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.black)
+                    .multilineTextAlignment(.center)
+                
+                Spacer()
+                
+                Button(action: {
+                    
+                    // TODO
+                        
+                }, label: {
+                    Image("apple")
+                        .renderingMode(.original)
+                        .resizable()
+                        .frame(width: 100.0, height: 100.0)
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(20)
+                        .shadow(color: /*@START_MENU_TOKEN@*/Color.gray/*@END_MENU_TOKEN@*/, radius: 3)
+                    
+                })
+                
+                Spacer()
+                
+            }
+        }
+    }
+}
+
+struct ContentView__Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
