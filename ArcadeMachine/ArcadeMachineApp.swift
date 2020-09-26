32751295846f70991f3c5c71523800dd2149913b
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-09-26 16:00:28 +0200

    Initial Commit

---
+++ ArcadeMachine/ArcadeMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArcadeMachineApp.swift
+//  ArcadeMachine
+//
+//  Created by Iorrah Gonzalez on 26.09.20.
+//
+
+import SwiftUI
+
+@main
+struct ArcadeMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArcadeMachine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ArcadeMachine
+//
+//  Created by Iorrah Gonzalez on 26.09.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
